440307091d96307e231c7d2080ff945abcd4c0e9
ma-gis <mahtab.gis@gmail.com>
2024-06-29 21:31:59 -0500

    initial project

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import logging, os, requests, json, datetime
 from typing import Optional
-
 from fastapi import FastAPI
+from boxsdk import JWTAuth, Client
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    # box configuration
+    if os.path.exists('/etc/secrets/config.json'):
+        return {f'config json found in secrets'}
+    #     auth = JWTAuth.from_settings_file(os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.json'))
+    # client = Client(auth)
+    # service_account = client.user().get()
+    #return {f'The current user ID is {service_account}'}
+    return {f'config json not found in secrets'}
+
